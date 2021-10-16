41eb5bfc5a282ecb52683c6614585952e20ff92c
seydlitz2185 <y188428292905@163.com>
2021-10-16 15:45:33 +0800

    Update lab6: idea

--- cs61bcodes/lab6/WQULauncher.java
+++ cs61bcodes/lab6/WQULauncher.java
@@ -1,20 +1,21 @@
 public class WQULauncher {
     public static void main(String[] args){
-        WeightedQuickUnion w = new WeightedQuickUnion(10);
-        int i = 1;
-        while (i == 1 || i<20){
-            w.union((int)(10*Math.random()),(int)(10*Math.random()));
-            i++;
-        }
+        WeightedQuickUnion w = new WeightedQuickUnion(9);
+//        int i = 1;
+//        while (i == 1 || i<20){
+//            w.union((int)(10*Math.random()),(int)(10*Math.random()));
+//            i++;
+//        }
 
-//        w.union(0,1);
-//        w.union(3,1);
-//        w.union(2,1);
-//        w.union(8,9);
-//        w.union(7,9);
-//        w.union(6,9);
-//        w.union(9,1);
-//        w.find(6);
+        w.union(2,3);
+        w.union(1,2);
+        w.union(5,7);
+        w.union(8,4);
+        w.union(7,2);
+        w.union(0,6);
+        w.union(6,4);
+        w.union(6,3);
+
 
     }
 }
